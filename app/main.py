56cae37ad35a3c25a27c6f9d2aae4d6fd6f313e2
SoSaymon <szymon.chirowski@protonmail.com>
2024-02-18 02:01:16 +0100

    feat: add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from graphene import Schema
+from starlette.middleware.cors import CORSMiddleware
 from starlette_graphene3 import GraphQLApp, make_playground_handler
 
 from app.database.utils.load_data import create_database
@@ -9,6 +10,20 @@ from app.gql.queries import Query
 app = FastAPI()
 
 schema = Schema(query=Query, mutation=Mutation)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["OPTIONS", "GET", "POST"],
+    allow_headers=["*"],
+)
 
 
 # @app.on_event("startup")
