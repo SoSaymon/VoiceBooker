5f90acb0910aa4a8c15f6551e980229e032fcac8
Aleeza Rubab <130530849+aleeza23@users.noreply.github.com>
2024-02-23 12:39:45 -0800

    Update App.js

--- vb-frontend/src/App.js
+++ vb-frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './App.css'
 import { Routes, BrowserRouter, Route } from 'react-router-dom'
-import Login from "./components/pages/Login";
 import Home from "./components/pages/Home";
 import Signup from "./components/pages/Signup";
 
@@ -9,6 +8,7 @@ import AuthProvider from "react-auth-kit";
 import createStore from "react-auth-kit/createStore";
 import RequireAuth from '@auth-kit/react-router/RequireAuth'
 import { refresh } from "./RefreshToken";
+import Signin from "./components/pages/Signin";
 
 const App = () => {
   const store = createStore({
@@ -16,7 +16,7 @@ const App = () => {
     authType: 'cookie',
     cookieDomain: window.location.hostname,
     cookieSecure: window.location.protocol === 'https:',
-    refresh : refresh
+    refresh: refresh
   });
   return <>
 
@@ -24,9 +24,9 @@ const App = () => {
     <AuthProvider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<RequireAuth fallbackPath={'/'} ><Home /></RequireAuth>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
